68cffcbc828daeea321cc8c29de7fbf9411ad12a
Tim Helmke <timhelmke@aol.com>
2021-10-06 14:00:02 +0200

    feature: add todos to list and logical check

--- script.js
+++ script.js
@@ -2,8 +2,40 @@ console.log("Hello Todo App!");
 
 function addNewTodo() {
   const newTodoEl = document.querySelector("#new-todo");
-  const newTodo = newTodoEl.value;
-  console.log("Your new todo: " + newTodo);
+  const newTodo = newTodoEl.value.trim();
+
+  if (newTodo.lenght === 0) {
+    return;
+  }
+
+  if (isDuplication(newTodo)) {
+    alert("This task is already in your list");
+    return;
+  }
+
+  const newTodoLi = document.createElement("li");
+  newTodoLi.innerText = newTodo;
+
+  newTodoLi.setAttribute("data-todo", newTodo.toLowerCase());
+
+  const todoListEl = document.querySelector("#todo-list");
+  todoListEl.appendChild(newTodoLi);
+
+  newTodoEl.value = "";
+}
+
+function isDuplication(todo) {
+  todo = todo.toLowerCase();
+  const todoListEl = document.querySelector("#todo-list");
+  for (let i = 0; i < todoListEl.children.length; i++) {
+    const currentLiEl = todoListEl.children[i];
+    const currentTodo = currentLiEl.getAttribute("data-todo");
+    if (currentTodo === todo || currentTodo === " ") {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 const addTodoBtn = document.querySelector("#add-todo");
